38777f5191c53615786aee3a09b7da199367188b
0seo8 <00seo23@gmail.com>
2023-12-20 15:27:13 +0900

    feat: 클라이언트 컴포넌트

--- note/src/app/page.tsx
+++ note/src/app/page.tsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import os from "os";
+import { Counter } from "@/components/Counter";
 
 export default function Home() {
   console.log(os.hostname());
 
   //but useState는 사용이 불가능
-  return <h1>홈페이지다!!</h1>;
+  return (
+    <>
+      <h1>홈페이지다!!</h1>
+      <Counter />
+    </>
+  );
 }
