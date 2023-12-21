9c3b16ea51db35200f06689abb3f3956bb061d7e
0seo8 <00seo23@gmail.com>
2023-12-21 17:49:14 +0900

    feat: images

--- note/src/app/products/page.tsx
+++ note/src/app/products/page.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import clothesImage from "../../../public/images/clothes.jpg";
 import { getProducts } from "@/service/prdoucts";
 import MeowArticle from "@/components/MeowArticle";
 
 export default async function ProductsPage() {
-  throw new Error();
   const products = await getProducts();
 
   return (
     <>
       <h1>제품 소개 페이지!</h1>
+      <Image src={clothesImage} alt="Clothes" />
       <ul>
         {products.map(({ id, name }, index) => (
           <li key={index}>
