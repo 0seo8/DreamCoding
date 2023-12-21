bd5b684fdb2c79027424aa9416fc16cc0ffb5fd7
0seo8 <00seo23@gmail.com>
2023-12-21 10:27:37 +0900

    feat: ssg렌더링

--- note/src/app/products/[slug]/page.tsx
+++ note/src/app/products/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { getProduct, getProducts } from "@/service/prdoucts";
+import { getProduct, getProducts, Product } from "@/service/prdoucts";
 import { notFound } from "next/navigation";
 
 type Props = {
@@ -7,20 +7,20 @@ type Props = {
     slug: string;
   };
 };
-export default function PantsPage({ params: { slug } }: Props) {
-  const product = getProduct(slug);
+export default async function ProductPage({ params: { slug } }: Props) {
+  const product = await getProduct(slug);
 
   if (!product) {
     notFound();
   }
   //서버 파일에 있는 데이터 중 해당 제품의 정보를 찾아서 그걸 보여줌
-  return <h1>{product} 제품 설명 페이지</h1>;
+  return <h1>{product.name} 제품 설명 페이지</h1>;
 }
 
-export function generateStaticParams() {
+export async function generateStaticParams() {
   // 모든 제품의 페이지들을 미리 만들어 둘 수 있게 해줄거임(SSG)
-  const products = getProducts();
+  const products = await getProducts();
   return products.map((product) => ({
-    slug: product,
+    slug: product.id,
   }));
 }
