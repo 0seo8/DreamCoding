cc4b12c58a91a2d6a3f22e1f017abd3eb27eb801
youngseo <00seo23@gmail.com>
2024-01-06 16:37:27 +0900

    feat: 페이지 레이아웃 만들기
    기본적인 골격설정

---
+++ blog/src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function AboutPage() {
+  return (
+  <p>
+   나에대해!
+  </p>
+ );
+}
---
+++ blog/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+  <p>
+    {"Don't forget to CODE your DREAM | ALL Right Reserved."}
+  </p>
+ );
+}
