230c11476257f04f94cbde413150df88f0d86218
youngseo <00seo23@gmail.com>
2024-01-07 21:50:55 +0900

    feat: 피쳐된 포스트 보여주기- 스타일링

--- blog/src/components/PostCard.tsx
+++ blog/src/components/PostCard.tsx
@@ -10,11 +10,18 @@ type Props = {
 export default function PostCard({post: {title, path, description, featured, date, category}}:Props) {
   return (
   <Link href={`/post/${path}`}>
-    <Image src={`/images/posts/${path}.png`} alt={title} width={300} height={200} />
-    <div><time>{date.toString()}</time></div>
-    <h3>{title}</h3>
-    <p>{description}</p>
-    <span>{category}</span>
+   <article className="rounded-md overflow-hidden shadow-lg">
+     <Image className="w-full" src={`/images/posts/${path}.png`} alt={title} width={300} height={200} />
+     <div className="flex flex-col items-center p-4">
+       <time className="self-end">{date.toString()}</time>
+     <h3 className="text-lg font-bold">{title}</h3>
+     {/*truncate : 한줄이 넘어가면 자동으로 잘라지게 함 단, w-full이 적용된 상태여야지 적용이 됨.*/}
+     <p className="w-full truncate text-center">{description}</p>
+     <span className="text-sm rounded-lg bg-green-100 px-2 my-2">
+       {category}
+     </span>
+     </div>
+   </article>
   </Link>
  );
 }
