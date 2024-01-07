517b23387d7afc99c977f388de548df0cd4772d9
youngseo <00seo23@gmail.com>
2024-01-07 23:53:43 +0900

    feat: 필터 가능한 포스트 페이지- 필터

--- blog/src/app/posts/page.tsx
+++ blog/src/app/posts/page.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { getAllPosts } from '@/service/posts';
+import FilterablePosts from '@/components/FilterablePosts';
 
-export default function PostsPage() {
+export default async function PostsPage() {
+  const posts = await getAllPosts()
+  const categories = [...new Set(posts.map(post => post.category))]
+
   return (
-  <p>
-   포스트들!
-  </p>
+  <FilterablePosts posts={posts} categories={categories}/>
  );
 }
---
+++ blog/src/components/FilterablePosts.tsx
@@ -0,0 +1,28 @@
+//카테고리라는 상태를 가지기 때문에 클라이언트 컴포넌트가 되어야 함.
+'use client'
+import React, { useState } from 'react';
+import { Post } from '@/service/posts';
+import PostsGrid from '@/components/PostsGrid';
+import Categories from '@/components/Categories';
+
+type Props = {
+  posts: Post[];
+  categories: string[];
+}
+
+const ALL_POSTS = 'All Posts'
+
+export default function FilterablePosts({posts, categories}:Props) {
+  const [selected, setSelected] = useState(ALL_POSTS)
+  const filtered = selected === ALL_POSTS ? posts : posts.filter(post => post.category===selected)
+  return (
+  <section>
+    <PostsGrid posts={filtered} />
+    <Categories
+      categories={[ALL_POSTS, ...categories]}
+      selected={selected}
+      onClick={setSelected}
+    />
+  </section>
+ );
+}
