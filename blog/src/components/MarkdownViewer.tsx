186d1261fe6862476d0004d88a5bd265dc9be88e
youngseo <00seo23@gmail.com>
2024-01-08 23:51:29 +0900

    feat: 마크다운뷰어-코드문법

--- blog/src/components/MarkdownViewer.tsx
+++ blog/src/components/MarkdownViewer.tsx
@@ -1,12 +1,48 @@
+// 'use client'
+
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
+import {materialDark} from 'react-syntax-highlighter/dist/esm/styles/prism'
+import Image from 'next/image';
 
 type Props = {
   content: string
 }
 export default function MarkdownViewer({content}: Props) {
   return (
-  <ReactMarkdown className="prose lg:prose-xl" remarkPlugins={[remarkGfm]}>
+  <ReactMarkdown
+    className="prose max-w-none"
+    remarkPlugins={[remarkGfm]}
+    components={{
+       code({children, className, node, ref, ...props}) {
+         const match = /language-(\w+)/.exec(className || '')
+         return match ? (
+           <SyntaxHighlighter
+             language={match[1]}
+             PreTag="div"
+             {...props}
+             style={materialDark}
+           >
+             {String(children).replace(/\n$/, '')}
+           </SyntaxHighlighter>
+         ) : (
+           <code {...props} className={className}>
+             {children}
+           </code>
+         )
+       },
+      img: (image) => (
+        <Image
+          className='w-full max-h-60 object-cover'
+          src={image.src || ''}
+          alt={image.alt || ''}
+          width={500}
+          height={350}
+        />
+      ),
+    }}
+  >
     {content}
   </ReactMarkdown>
  );
