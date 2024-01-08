a2ef142f54fee4b66be2c6afa73a49e606965112
youngseo <00seo23@gmail.com>
2024-01-08 18:20:40 +0900

    feat: 마크다운 뷰어-라이브러리

--- blog/src/app/posts/[slug]/page.tsx
+++ blog/src/app/posts/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { getPostData } from '@/service/posts';
+import MarkdownViewer from '@/components/MarkdownViewer';
 
 type Props = {
   params: {
@@ -8,14 +9,12 @@ type Props = {
 }
 
 export default async  function PostPage({params: {slug}}:Props) {
-  // 1. 전달된 slug에 해당하는 포스트 데이터를 읽어와서
-  // 2. 데이터를 마크다운뷰어에 표기하면 됨.
-
   const post = await getPostData(slug);
+
   return (
   <>
     <h1>{post.title}</h1>
-    <pre>{post.content}</pre>
+    <MarkdownViewer content={post.content}/>
   </>
  );
 }
