a326df086935c1f93350602a37226a9febef31f8
youngseo <00seo23@gmail.com>
2024-01-09 15:08:52 +0900

    feat: 다음 포스트 카드- 컴포넌트

--- blog/src/app/posts/[slug]/page.tsx
+++ blog/src/app/posts/[slug]/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { getPostData } from '@/service/posts';
 import Image from 'next/image'
 import PostContent from '@/components/PostContent';
+import AdjacentPostCard from '@/components/AdjacentPostCard';
 
 type Props = {
   params: {
@@ -16,9 +17,9 @@ export default async  function PostPage({params: {slug}}:Props) {
   <article className="rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4">
     <Image className="w-full h-1/5 max-h-[500px]" src={`/images/posts/${path}.png`} alt={title} width={760} height={420} />
     <PostContent post={post} />
-    <section>
-      {next && <p>{next.title}</p>}
-      {prev && <p>{prev.title}</p>}
+    <section className="flex shadow-md">
+      {prev &&<AdjacentPostCard post={prev} type={"prev"} />}
+      {next &&<AdjacentPostCard post={next} type={"next"} />}
     </section>
   </article>
  );
