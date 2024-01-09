af314689fc369706c65b84ebea0fcdfd385adb73
youngseo <00seo23@gmail.com>
2024-01-09 11:08:08 +0900

    feat: 다음 포스트 카드- 리팩토링

--- blog/src/app/posts/[slug]/page.tsx
+++ blog/src/app/posts/[slug]/page.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { getPostData } from '@/service/posts';
-import MarkdownViewer from '@/components/MarkdownViewer';
 import Image from 'next/image'
-import { AiTwotoneCalendar } from "react-icons/ai";
+import PostContent from '@/components/PostContent';
 
 type Props = {
   params: {
@@ -11,21 +10,11 @@ type Props = {
 }
 
 export default async  function PostPage({params: {slug}}:Props) {
-  const {title, content, description, date, path} = await getPostData(slug);
-
+  const post = await getPostData(slug);
   return (
   <article className="rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4">
-    <Image className="w-full h-1/5 max-h-[500px]" src={`/images/posts/${path}.png`} alt={title} width={760} height={420} />
-    <section className="flex flex-col p-4">
-      <div className="flex items-center self-end text-sky-600">
-        <AiTwotoneCalendar/>
-        <p className="font-semibold ml-2">{date.toString()}</p>
-      </div>
-      <h1 className="text-4xl font-bold">{title}</h1>
-      <p className="text-xl font-bold">{description}</p>
-      <div className="w-44 border-2 border-sky-600 mt-4 mb-8" />
-      <MarkdownViewer content={content}/>
-    </section>
+    <Image className="w-full h-1/5 max-h-[500px]" src={`/images/posts/${post.path}.png`} alt={post.title} width={760} height={420} />
+    <PostContent post={post} />
   </article>
  );
 }
