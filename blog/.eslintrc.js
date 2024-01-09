50c660484de696297374d7eac0ba0b19e5711b6e
youngseo <00seo23@gmail.com>
2024-01-09 21:47:55 +0900

    feat: 정보, 연락페이지-입력폼

---
+++ blog/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:react/recommended', 'next/core-web-vitals', 'prettier'],
+};
--- blog/src/components/ContactForm.tsx
+++ blog/src/components/ContactForm.tsx
@@ -1,9 +1,62 @@
-import React from 'react';
+'use client';
 
+import { ChangeEvent, FormEvent, useState } from 'react';
+
+type From = {
+  from: string;
+  subject: string;
+  message: string;
+};
 export default function ContactForm() {
+  const [form, setForm] = useState<From>({
+    from: '',
+    subject: '',
+    message: '',
+  });
+
+  const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(form);
+  };
+
   return (
-  <div>
-   
-  </div>
- );
+    <>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="from">Your Email</label>
+        <input
+          type="email"
+          id="from"
+          name="from"
+          required
+          autoFocus
+          value={form.from}
+          onChange={onChange}
+        />
+        <label htmlFor="subject">Subject</label>
+        <input
+          type="text"
+          id="subject"
+          name="subject"
+          required
+          value={form.subject}
+          onChange={onChange}
+        />
+        <label htmlFor="message">Message</label>
+        <textarea
+          rows={10}
+          id="message"
+          name="message"
+          required
+          value={form.message}
+          onChange={onChange}
+        />
+        <button>Submit</button>
+      </form>
+    </>
+  );
 }
