60293f0d3349f8ac8aa2f08c976828f5c50e1689
youngseo <00seo23@gmail.com>
2024-01-09 13:38:51 +0900

    feat: 다음 포스트 카드 - 로직구현

--- blog/src/app/posts/[slug]/page.tsx
+++ blog/src/app/posts/[slug]/page.tsx
@@ -11,10 +11,15 @@ type Props = {
 
 export default async  function PostPage({params: {slug}}:Props) {
   const post = await getPostData(slug);
+  const {title, path, next, prev} = post
   return (
   <article className="rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4">
-    <Image className="w-full h-1/5 max-h-[500px]" src={`/images/posts/${post.path}.png`} alt={post.title} width={760} height={420} />
+    <Image className="w-full h-1/5 max-h-[500px]" src={`/images/posts/${path}.png`} alt={title} width={760} height={420} />
     <PostContent post={post} />
+    <section>
+      {next && <p>{next.title}</p>}
+      {prev && <p>{prev.title}</p>}
+    </section>
   </article>
  );
 }
